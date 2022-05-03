43083ce9f9e0809869940debf05add0a0f9aef4a
Medi King's <jehovahmbote3@gmail.com>
2022-05-03 15:19:29 +0200

    Furst commit

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+
+    // Initialisation AOS Library
+    AOS.init({
+        delay: 100,
+        duration: 2000,
+        mirror: false,
+        once: true,
+    });
+})
